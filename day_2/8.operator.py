4a92ec7f9ab2c58d27928a6bc8414c7b10436b1b
manisha5nov <manishakumari5nov@gmail.com>
2026-08-12 18:49:41 +0530

    day_2

---
+++ day_2/8.operator.py
@@ -0,0 +1,10 @@
+# type of operator in python
+# 1. Arithmetic Operators: Used for mathematical operations like addition, subtraction, multiplication, division, etc
+# 2. Comparison Operators: Used to compare values and return a boolean result (True or False).
+# 3. Logical Operators: Used to combine conditional statements and return a boolean result.
+# 4. Assignment Operators: Used to assign values to variables.
+# 5. Bitwise Operators: Used to perform bitwise operations on integers.
+# 6. Membership Operators: Used to test if a value is present in a sequence (like a list, tuple, or string).
+# 7. Identity Operators: Used to compare the memory locations of two objects to determine if they are the same object in memory.  
+# 8. Ternary Operator: A shorthand way of writing an if-else statement in a single line.
+# 9. 
--- day_2/day_1/conversion.py
+++
@@ -1,9 +0,0 @@
-'''# type of conversion in python
-# 1. Implicit Conversion (Type Casting)
-# x=10
-# y=2.5
-# z=x+y  # Implicit conversion from int to float
-
-# 2. Explicit Conversion (Type Conversion)
-# z=int(y)  # Convert float to int
-'''
