23460961ae3e225f9ca9614d956cb5a05d36c4fb
Manisha_Singh <manishakumari5nov@gmail.com>
2026-08-17 09:47:24 +0530

    Delete day_6/66.q_50.py

--- day_6/66.q_50.py
+++
@@ -1,9 +0,0 @@
-# only even number product
-num=int(input("enter a number = "))
-mul=1
-while num>0:
-    r=num%10
-    if r%2==0:
-        mul*=r
-    num//=10
-print(f"multipaly of even digit {mul}")
