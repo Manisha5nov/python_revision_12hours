4cb013e130655325cc07b347d99b5fa3c54da97e
Manisha_Singh <manishakumari5nov@gmail.com>
2026-08-17 09:45:33 +0530

    Delete day_6/56.q_40.py

--- day_6/56.q_40.py
+++
@@ -1,9 +0,0 @@
-# factorial of a number using while loop
-
-num = int(input("Enter a number: "))
-factorial = 1
-i = 1
-while i <= num:
-    factorial *= i
-    i += 1
-print(f"The factorial of {num} is: {factorial}")
