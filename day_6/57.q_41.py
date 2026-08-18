c55fd8b88ed170b19706d944c65b4c8d1218c1a3
Manisha_Singh <manishakumari5nov@gmail.com>
2026-08-18 10:09:27 +0530

    Add files via upload

---
+++ day_6/57.q_41.py
@@ -0,0 +1,9 @@
+# ask a number from the user and count factors of that number using while loop
+num = int(input("Enter a number: "))
+count = 0
+i=1
+while i <= num:
+    if num % i == 0:
+        count += 1
+    i += 1
+print(f"The number of factors of {num} is: {count}")
