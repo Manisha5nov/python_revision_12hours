9a536200365a9eb0e98007b5845a1a2b25791f1e
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-03 10:22:06 +0530

    Delete day_8/97.q_75.py

--- day_8/97.q_75.py
+++
@@ -1,9 +0,0 @@
-# 1 2 3 4
-# 1 2 3 4
-# 1 2 3 4
-# 1 2 3 4    printing the pattern
-n=int(input("enter a number = "))
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        print(j,end=" ")
-    print()
