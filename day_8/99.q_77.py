d96be595556ed171d2db14d5c9fc8b1548113dde
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-05 10:23:19 +0530

    Add files via upload

---
+++ day_8/99.q_77.py
@@ -0,0 +1,10 @@
+# 11111
+# 22222
+# 33333
+# 44444  print the pattern
+
+n=int(input("enter a number = "))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(i,end=" ")
+    print()
