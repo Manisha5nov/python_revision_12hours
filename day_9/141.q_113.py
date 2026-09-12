97160217ee0a888c51a8578bf5da97e416606be6
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-12 16:58:09 +0530

    Delete day_9/141.q_113.py

--- day_9/141.q_113.py
+++
@@ -1,9 +0,0 @@
-# Write a function to count digits in a number.
-def count_digits(num):
-    count = 0
-    while num > 0:
-        num //= 10
-        count += 1
-    return count
-n=int(input("Enter a number: "))
-print(count_digits(n))
