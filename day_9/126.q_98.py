5461324219f7c9a5e6df03147d7ec18388f237c9
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-12 17:01:49 +0530

    Delete day_9/126.q_98.py

--- day_9/126.q_98.py
+++
@@ -1,9 +0,0 @@
-# Write a function to divide two numbers.
-def divide(a,b):
-    if b == 0:
-        return "Error: Division by zero is not allowed."
-    return a/b
-
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-print(divide(a, b))
