414944b5a2284222d11c25facb37e7aac4c5843f
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-16 10:26:31 +0530

    Add files via upload

---
+++ day_9/137.q_109.py
@@ -0,0 +1,17 @@
+# Write a function to print prime numbers in a range.
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def print_primes_in_range(start, end):
+    for num in range(start, end + 1):
+        if is_prime(num):
+            print(num, end=" ")
+
+start = int(input("Enter the start of the range: "))
+end = int(input("Enter the end of the range: "))
+print_primes_in_range(start, end)
