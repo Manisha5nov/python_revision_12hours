6e8034031988e631c4ccf55aa8a0bf355cc9a255
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-21 11:45:07 +0530

    Add string documentation in 149.string.py
    Added documentation explaining strings in Python.

---
+++ day_10/149.string.py
@@ -0,0 +1,3 @@
+# What is a string in Python?
+# A string in Python is a sequence of characters enclosed in single quotes (''), double quotes (""), or triple quotes (''' '''). Strings are immutable, meaning their contents cannot be changed after creation.  
+
