5768b3962491a1f6b5480d2b637147f69c03ed8b
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-21 11:45:32 +0530

    Create 150.create_string.py with string examples
    Added examples of string creation in Python.

---
+++ day_10/150.create_string.py
@@ -0,0 +1,9 @@
+# How do you create a string in Python?
+# You can create a string in Python by enclosing a sequence of characters in single quotes (' '), 
+# double quotes (" "), or triple quotes (''' ''' or """ """). Here are some examples:
+# ```python
+# string1 = 'Hello, World!'
+# string2 = "Python is great!"
+# string3 = '''This is a multi-line string.'''
+# string4 = """This is also a multi-line string."""
+# ```
