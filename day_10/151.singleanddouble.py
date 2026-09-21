20ac123c5adcc15157fab856ad4819133d825fcd
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-21 11:46:04 +0530

    Document differences between single and double quotes
    Added comments explaining the use of single and double quotes in Python strings.

---
+++ day_10/151.singleanddouble.py
@@ -0,0 +1,7 @@
+# What is the difference between single and double quotes?
+# In Python, both single quotes (' ') and double quotes (" ") can be used to create string literals.
+# There is no difference in functionality between the two, and you can use either based on your
+# preference or to avoid conflicts with quotes inside the string. For example:
+# ```python 
+# single_quote_string = 'Hello, World!'
+# double_quote_string = "Python is great!"
