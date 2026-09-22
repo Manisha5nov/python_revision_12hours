20004a8811b332f341d775a864a7a9663fa5ef00
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-22 10:21:21 +0530

    Add files via upload

---
+++ day_10/154.q_121.py
@@ -0,0 +1,7 @@
+# How do you access the first character of a string?
+
+# You can access the first character of a string in Python using indexing. Strings are indexed starting from 0, 
+# so the first character is at index 0. Here is an example:
+string = "Hello, World!"
+first_character = string[0]     
+print("The first character of the string is:", first_character)
