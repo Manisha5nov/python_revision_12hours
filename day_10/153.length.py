ac50d93e3afee70ab17d186ca51838d2d57bbdee
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-22 10:20:43 +0530

    Add files via upload

---
+++ day_10/153.length.py
@@ -0,0 +1,7 @@
+# How do you find the length of a string?
+# You can find the length of a string in Python using the built-in `len()` function. 
+# The `len()` function takes a string as an argument and returns the number of characters in that string, 
+# including spaces and special characters. Here is an example:
+string = "Hello, World!"    
+length = len(string)
+print("The length of the string is:", length)
