874eb00f349b4ba482e3c7b0cd8e032d225c9fe0
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-22 10:21:04 +0530

    Add files via upload

---
+++ day_10/152.triple.py
@@ -0,0 +1,7 @@
+# What are triple quotes used for?
+# Triple quotes (''' ''' or """ """) in Python are used for creating multi-line strings and for writing docstrings
+# (documentation strings) for functions, classes, and modules. They allow you to include line breaks and preserve 
+# the formatting of the text within the quotes. For example:
+# ```python
+# multi_line_string = '''This is a multi-line string.
+# It can span multiple lines.
