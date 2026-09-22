2aff6e4381aa4573d46ec796d7ddc9ef7ae9da0c
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-22 10:21:38 +0530

    Add files via upload

---
+++ day_10/155.q_122.py
@@ -0,0 +1,7 @@
+# How do you access the last character?
+# You can access the last character of a string in Python using negative indexing.
+# In Python, negative indices count from the end of the string, with -1 referring to the last character.
+# Here is an example:
+string = "Hello, World!"
+last_character = string[-1]
+print("The last character of the string is:", last_character)
