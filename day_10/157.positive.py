1311519eb47324c8ce99243efd2137edb8d56142
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-23 10:07:09 +0530

    Add files via upload

---
+++ day_10/157.positive.py
@@ -0,0 +1,8 @@
+# What is positive indexing?
+
+# Positive indexing in Python refers to accessing characters in a string using non-negative integer indices.
+# The first character of the string is at index 0, the second at index 1, and so on. 
+# This is the most common way to access characters in a string.
+
+# String:           P  Y  T  H  O  N
+# positive Index:   0  1  2  3  4  5
