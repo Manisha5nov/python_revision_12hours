8fe1f1a797b8a8730968a29e081685e3cda81e86
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-23 10:07:28 +0530

    Add files via upload

---
+++ day_10/158.negetive.py
@@ -0,0 +1,9 @@
+# What is negative indexing?
+# Negative indexing in Python refers to accessing characters in a string using negative integer indices.
+# The last character of the string is at index -1, the second last at index -2, and so on. This allows you to access
+# characters from the end of the string without needing to know its length.
+# For example, if you have a string `s = "Python"`, then `s[-1]` would return `'n'`, `s[-2]` would return `'o'`,
+# and so forth.
+
+# String:           P  Y  T  H  O  N
+# negative Index:  -6 -5 -4 -3 -2 -1
