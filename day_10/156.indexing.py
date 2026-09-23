383c4501e3e9d2ea302965c250e489ab23193ad6
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-23 10:06:19 +0530

    Add files via upload

---
+++ day_10/156.indexing.py
@@ -0,0 +1,11 @@
+# What is string indexing?
+
+# String indexing in Python refers to the process of accessing individual characters within a
+# string using numerical indices. Strings are zero-indexed, meaning the first character is at index 0,
+# the second at index 1, and so on. You can access a character at a specific index using square brackets `[]` 
+# after the string variable name. For example, if you have a string `s = "Hello"`, then `s[0]` would return `'H'`, 
+# `s[1]` would return `'e'`, and so forth.
+
+# String:           P  Y  T  H  O  N
+# positive Index:   0  1  2  3  4  5
+# negative Index:  -6 -5 -4 -3 -2 -1
