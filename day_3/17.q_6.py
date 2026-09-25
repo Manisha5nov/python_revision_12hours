6ebb54c4526e2c93e1be77b003ce03f3f952c790
Manisha_Singh <manishakumari5nov@gmail.com>
2026-09-25 11:22:59 +0530

    unsolve

--- day_3/17.q_6.py
+++ day_3/17.q_6.py
@@ -1,4 +1,4 @@
-        # take two number as input . without using * , calculate their product using addition and print the result.
+# take two number as input . without using * , calculate their product using addition and print the result.
 
 num1=int(input("enter first number ="))
 num2=int(input("enter second number ="))
